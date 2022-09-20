cd6a494efa050e6f7ee5d88ee069f25c5a298b79
ChairaWandau <87312908+ChairaWandau@users.noreply.github.com>
2022-09-20 19:20:29 +0300

    Update CUDA_lr1.cu

--- CUDA_lr1.cu
+++ CUDA_lr1.cu
@@ -2,56 +2,37 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-__global__ void Palindrom(int* a, int* arr, int count) {
-	int m = 0;
-	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	while (*a > 0)
+#define N (256)
+__global__ void Palindrom(int* a) {
+	int idx = (int)(blockDim.x * blockIdx.x + threadIdx.x);
+	int A = threadIdx.x*10;
+	int B = 0;
+	while (A != B)
 	{
-		m = m * 10 + *a % 10;
-		*a = *a / 10;
+		A += B;
+		B = A;
+		int m = 0;
+		while (B > 0)
+		{
+			m = m * 10 + B % 10;
+			B = B / 10;
+		}
+		B = m;
 	}
-	*a = m;
-	for (int i = idx-1; i >= 0; i--) {
-		arr[i] = *a % 10;
-		*a /= 10;
-	}
+	a[idx] = A;
 }
 int main(void) {
-	//Задаем значение числа, из которого будем получать полином (двузначное число)
-	int A=96;
-	int a=0; // host копия a
-	int* dev_a; // device копия a
-	//выделяем память для device копии a
-	cudaMalloc((void**)&dev_a, sizeof(int));
-	//До тех пор, пока число - не палиндром
-	while (A != a) {
-		A += a;
-		a = A;
-		// копируем ввод на device
-		cudaMemcpy(dev_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-		//считаем, сколько цифр в числе
-		int count = 0;
-		int n = A;
-		while (n != 0)
-		{
-			n = n / 10;
-			count++;
-		}
-		//создаем массив для цифр
-		int* arr = new int[count];
-		int* dev_arr;
-		cudaMalloc((void**)&dev_arr, count*sizeof(int));
-		// запускаем Palindrom() на GPU, передавая параметры
-		Palindrom <<< 8, 8 >>> (dev_a, dev_arr, count);
-		cudaMemcpy(arr, dev_arr, sizeof(int) * count, cudaMemcpyDeviceToHost);
-		//Превращаем массив в число
-		a=0;
-		for (int i = 0; i < count; i++)
-			a = a * 10 + arr[i];
+	int a[N];
+	int* dev = NULL;
+	cudaMalloc((void**)&dev, N * sizeof(int));
+	// запускаем Palindrom() на GPU, передавая параметры
+	Palindrom << <dim3(N/16, 1), dim3(16,1) >> > (dev);
+	// копируем результат функции обратно в a
+	cudaMemcpy(a, dev, N * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(dev);
+	for (int idx = 0; idx < N; idx++) 
+	{
+		printf("a[%d]=%d\n", idx, a[idx]);
 	}
-	// освобождаем память
-	cudaFree(dev_a);
-	// выводим результат на экран
-	printf("%i\n", A);
 	return 0;
 }
